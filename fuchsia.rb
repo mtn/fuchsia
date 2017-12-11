a7ba0c4f5ab7649fb32a497394b5593a3fe1bf5f
Michael Noronha <michaeltnoronha@gmail.com>
2017-12-10 19:38:18 -0500

    Add examples and repl/batch modes

---
+++ fuchsia.rb
@@ -0,0 +1,23 @@
+require 'readline'
+
+require_relative 'src/interpreter'
+
+
+if ARGV.size > 0
+    contents = File.open(ARGV[0],"r").read.chomp
+    lines = contents.split("\n")
+
+    for line in lines
+        p Interpreter.run(line)
+    end
+
+    return
+end
+
+while buf = Readline.readline('>> ', true)
+    begin
+        p Interpreter.run(buf)
+    rescue => e
+        puts "Error: '#{e.message}'"
+    end
+end
--- main.rb
+++
@@ -1,5 +0,0 @@
-require_relative 'src/parser'
-require_relative 'src/lexer'
-require_relative 'src/interpreter'
-
-interpreter = Interpreter.new
